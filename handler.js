15dba7a01398f0942611637efe7a2cf05c8fefc5
Brendan <bgmcmullen@comcast.net>
2024-05-15 18:56:42 -0400

    added queues

--- handler.js
+++ handler.js
@@ -1,8 +1,8 @@
 'use strict';
 
-function handlePackageDelivered(payload){
+function handlePackageDelivered(payload, storeName){
   console.log('----------------------');
-  console.log(`VENDOR SAYS: Thank you for your order ${payload.customer}`);
+  console.log(`${storeName} SAYS: Thank you for your order ${payload.customer}`);
 }
 
 module.exports = handlePackageDelivered;
